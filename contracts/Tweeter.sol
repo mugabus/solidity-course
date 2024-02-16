6285bb6ae78a29abafdb8aa3a8e78f26d39172b1
mugabus <fatakimugaruka91@gmail.com >
2024-02-16 16:33:34 +0200

    adding the array in the tweet section

--- contracts/Tweeter.sol
+++ contracts/Tweeter.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
-
 // 1️⃣ Create a Twitter Contract 
 // 2️⃣ Create a mapping between user and tweet 
 // 3️⃣ Add function to create a tweet and save it in mapping
@@ -9,16 +8,26 @@ pragma solidity ^0.8.22;
 // 5️⃣ Add array of tweets 
 
 contract Twitter {
-   
-  mapping (address => string) public tweets;
+    // Mapping to store tweets of each user
+    mapping(address => string[]) public tweets;
 
-  function createTweet(string memory _tweet) public {
-        // You had a typo here. It should be "_tweet" instead of "tweet".
-        tweets[msg.sender] = _tweet;
+    // Function to create a new tweet
+    function createTweet(string memory _tweet) public {
+        // Push the new tweet to the sender's array of tweets
+        tweets[msg.sender].push(_tweet);
     }
 
-   function getTweet(address _owner) public view returns (string memory) {
+    // Function to get a specific tweet of a user by index
+    function getTweet(address _owner, uint _i) public view returns (string memory) {
+        // Ensure the index is within bounds
+        require(_i < tweets[_owner].length, "Tweet index out of bounds");
+        // Retrieve and return the tweet at the specified index
+        return tweets[_owner][_i];
+    }
+
+    // Function to get all tweets of a user
+    function getAllTweets(address _owner) public view returns (string[] memory) {
+        // Return the array of tweets for the given user
         return tweets[_owner];
     }
-
 }
