eada0df728cca5bc3e0b387bbcae40f1803c3d67
mugabus <fatakimugaruka91@gmail.com >
2024-02-16 14:51:51 +0200

    second changes

--- contracts/hello.sol
+++ contracts/hello.sol
@@ -1,5 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
-contract Hello{
-    string public welcome="hello world";
+// 1️⃣ Make a contract called Calculator
+// 2️⃣ Create Result variable to store result
+// 3️⃣ Create functions to add, subtract, and multiply to result
+// 4️⃣ Create a function to get result
+
+contract Calculator {
+    uint256 public result;
+    
+    function add(uint256 num) public {
+        result +=num;
+    }
+     function subtract(uint256 num) public {
+        result -=num;
+     }
+
+    function multiply(uint256 num) public {
+        result *=num;
+    }
+    function get()public view returns (uint256){
+        return result;
+    }
+
 }
